b6732908de5b92d37224c4a5139ef44c2fb7a05b
MateuszBiernat2001 <84177161+MateuszBiernat2001@users.noreply.github.com>
2021-07-20 17:22:16 +0200

    Update js.js

--- js.js
+++ js.js
@@ -22,23 +22,21 @@ btn.addEventListener('click' ,handleNav)
 
 
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
